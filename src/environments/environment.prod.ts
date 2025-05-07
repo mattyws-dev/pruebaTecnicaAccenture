443560c1c95086762f1fd6a3cd4ea4c66e1872b1
mattyws-dev <owen.anguloc@gmail.com>
2025-05-07 15:28:01 -0500

    feat(firebase): agrega control de feature flag con Remote Config para filtro por categorías

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAOuyCCwGlCrE3jt7gyWDv4Q6wendxs9ek",
+    authDomain: "pruebatecnicaacceture.firebaseapp.com",
+    projectId: "pruebatecnicaacceture",
+    storageBucket: "pruebatecnicaacceture.firebasestorage.app",
+    messagingSenderId: "128855514752",
+    appId: "1:128855514752:web:347d2c2498838531fd1065"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAOuyCCwGlCrE3jt7gyWDv4Q6wendxs9ek",
+    authDomain: "pruebatecnicaacceture.firebaseapp.com",
+    projectId: "pruebatecnicaacceture",
+    storageBucket: "pruebatecnicaacceture.firebasestorage.app",
+    messagingSenderId: "128855514752",
+    appId: "1:128855514752:web:347d2c2498838531fd1065"
+  }
 };
 
 /*
